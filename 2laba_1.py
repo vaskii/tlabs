7d0c40ea9dc99703a0ca3650e7271cf6d3b25887
vaskii <47794046+vaskii@users.noreply.github.com>
2019-05-08 01:44:23 +0300

    Update 2laba_1.py

--- 2laba_1.py
+++ 2laba_1.py
@@ -1,6 +1,9 @@
-my_string = "Ф;И;О;Возраст;Категория;_Иванов;Иван;Иванович;23 года;Студент 3 курса;_Петров;Семен;Игоревич;22 года;Студент 2 курса"
-my_string=my_string.replace('_','')
-my_string = my_string.split(';')
+my_string = "Heard you say you wanna die, so do I " \
+"Just do it like Nike, eat the night " \
+"Heard you said you wanna play, then it's Fisher Price " \
+"Your shoes faker than my smile, not surprised"
+my_string=my_string.replace(',','')
+my_string = my_string.split(' ')
 my_list=[]
 k = 0
 while k < len(my_string):
